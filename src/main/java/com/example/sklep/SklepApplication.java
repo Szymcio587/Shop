b9a467558a68d7bcd8badd4e2729ebc69431d8be
Szymcio587 <szymon.talar2882@gmail.com>
2023-05-03 13:36:11 +0200

    RestController test passed; MongoDB URI fixed

--- src/main/java/com/example/sklep/SklepApplication.java
+++ src/main/java/com/example/sklep/SklepApplication.java
@@ -3,6 +3,7 @@ package com.example.sklep;
 import com.example.sklep.model.Game;
 import com.example.sklep.model.GameType;
 import com.example.sklep.repository.GameRepository;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,22 +19,6 @@ public class SklepApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SklepApplication.class, args);
-    }
-
-    public void run(String... args) {
-        AddGames();
-        ShowAllGames();
-    }
-
-    public void AddGames() {
-        System.out.println("Adding games...");
-        gameRepository.save(new Game(1L, "Grand Theft Auto V", GameType.ACTION, 100, 4.6, " "));
-        gameRepository.save(new Game(2L, "FIFA 23", GameType.SPORT, 220, 2.0, " "));
-        gameRepository.save(new Game(3L, "Dark Souls 3", GameType.ACTION, 250, 5.0, " "));
-    }
-
-    public void ShowAllGames() {
-        gameRepository.findAll().forEach(game -> System.out.print(game));
     }
 
 }
--- src/main/java/com/example/sklep/controller/TestController.java
+++ src/main/java/com/example/sklep/controller/TestController.java
@@ -1,21 +1,28 @@
 package com.example.sklep.controller;
 
+import com.example.sklep.model.Game;
+import com.example.sklep.model.GameType;
 import com.example.sklep.repository.GameRepository;
-import com.example.sklep.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
     @Autowired
-    private GameService gameService;
+    private GameRepository gameRepository;
 
-    @GetMapping
+    @GetMapping("/")
     public String Test() {
-        GameRepository gameRepository = new
-        gameService = new GameService();
-        gameService.
+        System.out.println("Adding games...");
+        gameRepository.save(new Game(1L, "Grand Theft Auto V", GameType.ACTION, 100, 4.6, " "));
+        gameRepository.save(new Game(2L, "FIFA 23", GameType.SPORT, 220, 2.0, " "));
+        gameRepository.save(new Game(3L, "Dark Souls 3", GameType.ACTION, 250, 5.0, " "));
+
+        Game returned = gameRepository.findById(1L).get();
+
+        return "The name of the first game is " + returned.getName();
     }
 }
